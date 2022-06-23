5fce36521ec9840a3f6d5b43ec6f1a5443427bbb
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-06-23 14:55:21 -0300

    integração com api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
 import { Header } from "./components/Header"
 import { Lesson } from "./components/Lesson"
 import { Sidebar } from "./components/Sidebar"
+import { client } from "./lib/apollo"
 import { Event } from "./pages/Event"
+import { Router } from "./Router"
 
 function App() {
   return (
-    <div>
-      <Event/>
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 

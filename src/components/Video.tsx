7090c41c9700cf830e28fc0ed52fdde26a27d18d
Thiago Mesquita <mesquita.thi@hotmail.com>
2022-06-23 09:17:35 -0300

    Adicinando video player

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,15 +1,22 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, FileImage, Lightning } from "phosphor-react";
+
+import "@vime/core/themes/default.css"
 
 export function Video() {
   return (
     <div className="flex-1">
       <div className="bg-white flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="SO4-izct7Mc"/>
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
-
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
               Aula 1 - Abertura do Evento
@@ -17,15 +24,16 @@ export function Video() {
             <p className="mt-4 text-gray-200 leading-relaxed">
               Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.
             </p>
+
             <div className="flex items-center gap-4 mt-6">
               <img className="h-16 w-16 rounded-full border-2 border-blue-500"
                 src="https://github.com/thiag0mesquita.png"
                 alt=""
               />
-            </div>
-            <div className="leading-relaxed">
-              <strong className="font-bold text-2xl block">Thiago Mesquita</strong>
-              <span className="text-gray-200 text-sm block">Dev Front-End</span>
+              <div className="leading-relaxed">
+                <strong className="font-bold text-2xl block">Thiago Mesquita</strong>
+                <span className="text-gray-200 text-sm block">Dev Front-End</span>
+              </div>
             </div>
           </div>
 
@@ -39,8 +47,8 @@ export function Video() {
               Acesse o desafio
             </a>
           </div>
-
         </div>
+
         <div className="gap-8 mt-20 grid grid-cols-2">
           <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
             <div className="bg-green-700 h-full p-6 flex items-center">
